eaf68fb875fdab1701dfdb92c125636f0bc8c2d8
Dan Koskei <qimtaid@gmail.com>
2024-04-03 00:55:31 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(userData)
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then(data => {
+        document.body.innerHTML += data.id;
+        return data;
+      })
+      .catch(error => {
+        console.error("Error:", error);
+        document.body.innerHTML += error.message;
+        return error;
+      });
+  }
+  
